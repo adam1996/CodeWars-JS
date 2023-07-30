f722b2043574af5b9fcbf4c092f7fcbfb14e518c
Adam Johnson <adam7819@googlemail.com>
2023-07-30 21:39:41 +0100

    Update

--- 7 Kyu/Find the capitals.js
+++ 7 Kyu/Find the capitals.js
@@ -3,6 +3,7 @@ Write a function that takes a single string (word) as argument. The function mus
 
 Example
 Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
 */
 var capitals = function (word) {
   let sol = [];
