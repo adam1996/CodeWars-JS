56116047fcb713ccb3990f65568314fe68380e87
Adam Johnson <adam7819@googlemail.com>
2023-08-15 22:19:13 +0100

    Refactored to use filter

--- 7 Kyu/Two numbers are positive.js
+++ 7 Kyu/Two numbers are positive.js
@@ -1,7 +1,11 @@
-function twoArePositive(a, b, c) {
-    let positive = 0;
-    if (a > 0) {positive += 1}
-    if (b > 0) {positive += 1}
-    if (c > 0) {positive += 1}
-    return positive === 2;
-  }
+// function twoArePositive(a, b, c) {
+//     let positive = 0;
+//     if (a > 0) {positive += 1}
+//     if (b > 0) {positive += 1}
+//     if (c > 0) {positive += 1}
+//     return positive === 2;
+//   }
+
+function twoArePositive(a, b, c){
+    return [...arguments].filter((i) => i > 0).length === 2; 
+}
